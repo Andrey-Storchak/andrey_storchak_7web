a3467f52321a11de361c8e853162b2ca70726210
Andrey Storchak <andrey.storchak@gmail.com>
2014-01-25 11:20:26 +0200

    Cheating

--- settings/settings_heroku.py
+++ settings/settings_heroku.py
@@ -3,3 +3,4 @@ import dj_database_url
 
 DEBUG=False
 DATABASES = {'default': dj_database_url.config()}
+ADMIN_MEDIA_PREFIX = 'https://github.com/django/django/tree/master/django/contrib/admin/static/'
