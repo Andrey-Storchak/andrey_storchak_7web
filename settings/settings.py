846e2bcb838da8c083adfe7d8608c2b5cb3d3d33
Andrey Storchak <andrey.storchak@gmail.com>
2014-01-25 11:29:00 +0200

    fixing collectstatic

--- settings/settings.py
+++ settings/settings.py
@@ -53,8 +53,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = (
-   'static/',
-   'admin/',
+   os.path.join(BASE_DIR, 'admin/'),
 )
 
 STATICFILES_FINDERS = (
