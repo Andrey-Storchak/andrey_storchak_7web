dcd5798d1698506a3c315a4c73db6e9c346db99c
Andrey Storchak <andrey.storchak@gmail.com>
2014-01-25 11:36:45 +0200

    fixing collectstatic

--- settings/settings_heroku.py
+++ settings/settings_heroku.py
@@ -1,6 +1,5 @@
 from .settings import *
 import dj_database_url
 
-DEBUG=False
+DEBUG = True
 DATABASES = {'default': dj_database_url.config()}
-ADMIN_MEDIA_PREFIX = 'https://github.com/django/django/tree/master/django/contrib/admin/static/'
