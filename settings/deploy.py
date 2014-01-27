701a083bcf330abbbeb29f58504d6f6a8b0de1fa
Andrey <andrey.storchak@gmail.com>
2014-01-27 12:19:18 +0200

    trying to fix admin static n9

--- settings/deploy.py
+++ settings/deploy.py
@@ -7,4 +7,10 @@ import dj_database_url
 DEBUG = False
 DATABASES = {'default': dj_database_url.config()}
 
-STATIC_ROOT = 'static/'
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, 'static'),
+)
