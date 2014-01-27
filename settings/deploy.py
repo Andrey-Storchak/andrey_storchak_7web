c41d82717131fb955c9e5395c350b205406227d1
Andrey <andrey.storchak@gmail.com>
2014-01-27 09:55:44 +0200

    t5: Fix admin static files

--- settings/deploy.py
+++ settings/deploy.py
@@ -7,7 +7,8 @@ import dj_database_url
 DEBUG = False
 DATABASES = {'default': dj_database_url.config()}
 
-STATIC_ROOT = 'static/'
+STATIC_ROOT = ''
 STATICFILES_DIRS = (
-    'admin/',
+    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'static/admin'),
     )
