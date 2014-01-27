7d8749571a16d946b778a3158653d34ba64c603b
Andrey <andrey.storchak@gmail.com>
2014-01-27 16:44:32 +0200

    Heroku deployment

--- andrey_storchak_test_7web/urls.py
+++ andrey_storchak_test_7web/urls.py
@@ -14,5 +14,5 @@ urlpatterns = patterns('',
     )
 
 urlpatterns += patterns('',
-(r'^static(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 )
