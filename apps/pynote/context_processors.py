1028d73b355dc61139c9a8dea5bec452a6c53043
Andrey-Storchak <adnel.git@gmail.com>
2014-01-28 07:54:27 +0200

    Update context_processors.py

--- apps/pynote/context_processors.py
+++ apps/pynote/context_processors.py
@@ -1,6 +1,7 @@
-from . import models
+from pynote.models import Note
 
 
 def notes_count(request):
-    count = models.Note.objects.count()
+    '''Returns total count of notes in db'''
+    count = Note.objects.count()
     return {'notes_count': count}
