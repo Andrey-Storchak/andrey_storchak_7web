2725f5fa164ce213d888ae56dd952c116529535e
Andrey Storchak <andrey.storchak@gmail.com>
2014-01-28 09:32:19 +0200

    t6: Updated test

--- apps/pynote/tests/test_forms.py
+++ apps/pynote/tests/test_forms.py
@@ -15,21 +15,26 @@ class TestAdditionForm(TestCase):
 
     def test_valid_add(self):
         response = self.client.post(reverse('add_note'),
-                                    self.v_note_creds)
-        self.assertRedirects(response, reverse('home'), 302, 200)
+                                    self.v_note_creds,
+                                    HTTP_X_REQUESTED_WITH='XMLHttpRequest')
 
         created_note = models.Note.objects.get(pk=1)
         self.assertEqual(created_note.title,
                          self.v_note_creds['title'].upper())
         self.assertEqual(created_note.text,
                          self.v_note_creds['text'])
+        self.assertIn('request_result', response.content)
+        self.assertIn('Note added successfuly.', response.content)
 
     def test_invalid_add(self):
         response = self.client.post(reverse('add_note'),
-                                    self.i_note_creds)
+                                    self.i_note_creds,
+                                    HTTP_X_REQUESTED_WITH='XMLHttpRequest')
         self.assertEqual(response.status_code, 200)
         with self.assertRaises(models.Note.DoesNotExist):
             models.Note.objects.get(pk=1)
+        self.assertIn('request_result', response.content)
+        self.assertIn('Error occured.', response.content)
 
 
 class TestUpperCharField(TestCase):
