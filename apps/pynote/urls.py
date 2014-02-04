188e42c59f53dc19022dd0145c53f24761af84d6
Andrey Storchak <andrey.storchak@gmail.com>
2014-02-04 18:51:26 +0200

    t8: Fixed javascript and content types

--- apps/pynote/urls.py
+++ apps/pynote/urls.py
@@ -8,6 +8,6 @@ from apps.pynote import views
 urlpatterns = patterns('',
     url(r'^$', views.HomeView.as_view(), name='home'),
     url(r'^/add$', views.AddNoteView.as_view(), name='add_note'),
-    url(r'^/widget$', views.WidgetView.as_view(), name="widget"),
+    url(r'^/widget$', views.WidgetView.as_view(content_type="text/javascript"), name="widget"),
 )
 
