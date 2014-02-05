05f63f874f7654ebb220c6bc6f093e2c24c5518a
Andrey Storchak <andrey.storchak@gmail.com>
2014-02-05 14:09:47 +0200

    t9: Added tests

---
+++ apps/pynote/tests/test_signals.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+from apps.pynote import models
+
+
+class TestBookModel(TestCase):
+
+    def setUp(self):
+        self.created_book = models.Book.objects.create(book_name="Example")
+        self.created_note = models.Note.objects.create(title="test", text="text")
+        self.created_book.notes.add(self.created_note)
+
+    def test_last_deletion(self):
+        self.created_note.objects.delete()
+        with self.assertRaises(models.Book.DoesNotExist):
+            models.Book.objects.get()
+
